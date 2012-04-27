bd70032a83a1bfbc7c0cae308bbe064351574809
Ben Bullock <benkasminbullock@gmail.com>
2012-04-27 12:58:40 +0900

    Rm cdb dependence from dictionary.py.

--- dictionary.py
+++ dictionary.py
@@ -4,13 +4,6 @@
 ##  dictionary.py - 
 ##
 
-# try to load cdb
-try:
-  import cdb
-except ImportError:
-  cdb = None
-
-
 ##  Dictionary
 ##
 class Dictionary:
@@ -18,11 +11,6 @@ class Dictionary:
   max_tokens = 3
 
   def __init__(self, fname, userdict=None):
-    if fname.endswith(".cdb"):
-      if not cdb:
-        raise RuntimeError("cdb is not supported.")
-      self.dict = cdb.init(fname)
-    else:
       self.dict = {}
       fp = file(fname)
       while True:
@@ -30,7 +18,7 @@ class Dictionary:
         if not s: break
         f = s.split("\t")
         self.dict[f[0]] = f[1]
-    return
+      return
 
   def lookup_string1(self, s):
     try:
